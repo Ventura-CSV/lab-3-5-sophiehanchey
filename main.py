48b14344a7c64618f02f17b0672f87ac085255b2
Kyu Lee <kwooler@gmail.com>
2023-05-21 23:30:50 -0700

    Lab 3-5

--- main.py
+++ main.py
@@ -1,15 +1,4 @@
 def main():
-    ##################################################
-    # Comlete your code here
-    ##################################################
-    pass
-
-
-if __name__ == '__main__':
-    main()
-
-
-def main():
     num1 = int(input('Enter the first number: '))
     num2 = int(input('Enter the second number: '))
     num3 = int(input('Enter the third number: '))
