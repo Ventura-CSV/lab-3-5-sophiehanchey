3f8ba6d63323308ba77bde289e2afcd906ffa8e2
Kyu Lee <kwooler@gmail.com>
2023-05-21 23:16:24 -0700

    Lab 3-5

---
+++ main.py
@@ -0,0 +1,18 @@
+def main():
+    num1 = int(input('Enter the first number: '))
+    num2 = int(input('Enter the second number: '))
+    num3 = int(input('Enter the third number: '))
+    """
+    ########################################
+    Code Your Program here
+    ########################################
+    """
+
+    ########################################
+    # Do not delete the return statement
+    ########################################
+    return duplication
+
+
+if __name__ == '__main__':
+    main()
