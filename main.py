a9d6abbd7a5c6ec497167eb0e27c5c8bf3783560
Sophie Hanchey <sophiehanchey@gmail.com>
2024-06-27 18:02:17 -0700

    pytest passed

--- main.py
+++ main.py
@@ -2,13 +2,22 @@ def main():
     num1 = int(input('Enter the first number: '))
     num2 = int(input('Enter the second number: '))
     num3 = int(input('Enter the third number: '))
-    """
-    ########################################
-    Code Your Program here
-    ########################################
-    """
-    duplication = 0
+    
 
+    if(num1 == num2):
+        if(num2 == num3):
+            duplication = 3
+        else:
+            duplication = 2
+    elif(num1 == num3):
+        duplication = 2
+    elif(num2 == num3):
+        duplication = 2
+    else:
+        duplication = 0
+        
+    print(duplication)
+        
     ########################################
     # Do not delete the return statement
     ########################################
